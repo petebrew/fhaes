5cc049339e310d4a3098e76a6af0156aeebea665
Elena <Elena@Elenas-HP>
2016-03-09 19:40:03 -0700

    #136fixing the filter for intervals analysis by sample

--- fhaes/src/main/java/org/fhaes/analysis/FHInterval.java
+++ fhaes/src/main/java/org/fhaes/analysis/FHInterval.java
@@ -81,7 +81,7 @@ public class FHInterval {
 	public FHInterval(FHFile[] inputFileArray, AnalysisType analysisType, Integer startYear, Integer endYear, FireFilterType filterType,
 			Double filterValue, Boolean includeIncomplete, EventTypeToProcess eventTypeToProcess, Double alphaLevel,
 			SampleDepthFilterType sampleDepthFilterType, Double sampleDepthFilterValue) {
-	
+			
 		if (inputFileArray == null || inputFileArray.length == 0)
 		{
 			log.error("FHInterval must be passed an input file array");
@@ -125,7 +125,7 @@ public class FHInterval {
 	 * @return
 	 */
 	public File getExceedence() {
-	
+		
 		return exceedenceFile;
 	}
 	
@@ -135,7 +135,7 @@ public class FHInterval {
 	 * @return
 	 */
 	public File getSummary() {
-	
+		
 		return summaryFile;
 	}
 	
@@ -144,7 +144,7 @@ public class FHInterval {
 	 */
 	@SuppressWarnings("deprecation")
 	private void doAnalysis() {
-	
+		
 		log.debug("INPUT PARAMETERS");
 		log.debug("inputFileArray = " + inputFileArray);
 		log.debug("analyissType = " + analysisType);
@@ -375,7 +375,7 @@ public class FHInterval {
 		
 		double[] fixvalt = { 0.999, 0.99, 0.975, 0.95, 0.9, 0.875, 0.8, 0.75, 0.7, 0.667, 0.5, 0.333, 0.3, 0.25, 0.2, 0.125, 0.1, 0.05,
 				0.025, 0.01, 0.001 };
-		
+				
 		double[][] ExceeProbcomp = new double[fixvalt.length][myReader.size()];
 		double[][] ExceeProbsample = new double[fixvalt.length][myReader.size()];
 		// log.debug("the size of statsparam is " +
@@ -530,8 +530,8 @@ public class FHInterval {
 						{
 							if (myReader.get(i).getRecordingDepths(eventTypeToProcess)[climateYear.indexOf(listYears.get(ij))] != 0)
 							{
-								percentOfRecordingfilter.add(new Double(climateVectorFilter2.get(0).get(
-										climateYear.indexOf(listYears.get(ij)))
+								percentOfRecordingfilter.add(new Double(climateVectorFilter2.get(0)
+										.get(climateYear.indexOf(listYears.get(ij)))
 										/ myReader.get(i).getRecordingDepths(eventTypeToProcess)[climateYear.indexOf(listYears.get(ij))]));
 							}
 							else
@@ -925,33 +925,58 @@ public class FHInterval {
 					// myReader.get(i).getYear().size());
 					// log.debug("years with fires in sample "+k +
 					// "years are ");
-					for (int j = 0; j < myReader.get(i).getYearArray().size(); j++)
+					// for (int j = 0; j < myReader.get(i).getYearArray().size(); j++)
+					for (int j = 0; j < listYears.size(); j++)
 					{
 						// log.debug("the size climate2d is "+myReader.get(i).getClimate2d().get(k).get(j));
 						if (eventTypeToProcess.equals(EventTypeToProcess.FIRE_EVENT))
 						{
-							if ((myReader.get(i).getClimate2d().get(k).get(j) == 1))
+							if (climateYear.indexOf(listYears.get(j)) != -1)
 							{
-								// log.debug("I here inside ==1 "+
-								// j+" "+myReader.get(i).getFirstYear());
-								// int temp=j+myReader.get(i).getFirstYear();
-								// log.debug((j+myReader.get(i).getFirstYear()));
-								FyearperSampletemp.add((j + myReader.get(i).getFirstYear()));
+								if (myReader.get(i).getClimate2d().get(k).get(climateYear.indexOf(listYears.get(j))) == 1)
+								{
+									FyearperSampletemp.add((j + myReader.get(i).getFirstYear()));
+								}
 							}
+							
 						}
+						// {
+						// if ((myReader.get(i).getClimate2d().get(k).get(j) == 1))
+						// {
+						/// log.debug("I here inside ==1 "+
+						/// j+" "+myReader.get(i).getFirstYear());
+						/// int temp=j+myReader.get(i).getFirstYear();
+						/// log.debug((j+myReader.get(i).getFirstYear()));
+						///// FyearperSampletemp.add((j + myReader.get(i).getFirstYear()));
+						// }
+						// }
 						else if (eventTypeToProcess.equals(EventTypeToProcess.INJURY_EVENT))
 						{
-							if ((myReader.get(i).getClimate2dII().get(k).get(j) == 1))
+							if (climateYear.indexOf(listYears.get(j)) != -1)
 							{
-								FyearperSampletemp.add((j + myReader.get(i).getFirstYear()));
+								if (myReader.get(i).getClimate2dII().get(k).get(climateYear.indexOf(listYears.get(j))) == 1)
+								{
+									FyearperSampletemp.add((j + myReader.get(i).getFirstYear()));
+								}
 							}
+							// if ((myReader.get(i).getClimate2dII().get(k).get(j) == 1))
+							// {
+							// FyearperSampletemp.add((j + myReader.get(i).getFirstYear()));
+							// }
 						}
 						else if (eventTypeToProcess.equals(EventTypeToProcess.FIRE_AND_INJURY_EVENT))
 						{
-							if ((myReader.get(i).getClimate2dIII().get(k).get(j) == 1))
+							if (climateYear.indexOf(listYears.get(j)) != -1)
 							{
-								FyearperSampletemp.add((j + myReader.get(i).getFirstYear()));
+								if (myReader.get(i).getClimate2dIII().get(k).get(climateYear.indexOf(listYears.get(j))) == 1)
+								{
+									FyearperSampletemp.add((j + myReader.get(i).getFirstYear()));
+								}
 							}
+							// if ((myReader.get(i).getClimate2dIII().get(k).get(j) == 1))
+							// {
+							// FyearperSampletemp.add((j + myReader.get(i).getFirstYear()));
+							// }
 						}
 						else
 						{
@@ -1051,7 +1076,7 @@ public class FHInterval {
 					// ADDED BY PETE
 					if (dfireintervalspersample.length == 0)
 						continue;
-					
+						
 					/*
 					 * load the Summary Analysis for the Sample fire intervals
 					 */
@@ -1182,7 +1207,7 @@ public class FHInterval {
 				}
 			} // end of if jRadioSample selected.
 				// log.debug("the size of exceeprobsample is "ExceeProbsample.length+" X "+ExceeProbsample[0].length);
-		} // end of i readering each file loop do loop (354-465)
+		} // end of i readering each file loop do loop (354-1185)
 		/*
 		 * 
 		 */
@@ -1430,13 +1455,13 @@ public class FHInterval {
 		}
 		finally
 		{
-			
+		
 		}
 		
 	}
 	
 	public static ArrayList<Integer> generateFireIntervals(ArrayList<Integer> YearsWithFires) {
-	
+		
 		ArrayList<Integer> fireIntervals = new ArrayList<Integer>();
 		for (int i = 0; i < YearsWithFires.size() - 1; i++)
 		{
